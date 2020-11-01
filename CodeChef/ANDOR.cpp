6ff2c79972a2554aca571d08af4558a668934020
unknown <praful.mohanan@gmail.com>
2020-11-01 15:51:39 +0530

    Add ANDOR

---
+++ CodeChef/ANDOR.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define oset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+#define nl "\n";
+#define loop(from,to) for(int i=from;i<to;i++)
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+int main()
+{   
+    OJ;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll x;
+        cin >> x;
+        cout<<1<<" "<<x-1<<nl
+    }
+    return 0;
+}
+
