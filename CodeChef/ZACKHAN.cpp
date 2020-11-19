37206f817802d2e8c4c50a33d11f8b77b18dda72
unknown <praful.mohanan@gmail.com>
2020-11-19 22:53:30 +0530

    Add ZACKHAN

---
+++ CodeChef/ZACKHAN.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define oset tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+#define nl "\n";
+#define loop(from,to) for(int i=from;i<to;i++)
+#define OJ \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define fastio \
+    ios::sync_with_stdio(0); \
+    cin.tie(0); \
+    cout.tie(0);
+
+int main()
+{   
+    OJ;
+    fastio;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int l, b;
+        cin >> l >> b;
+        cout<<__gcd(l,b)<<nl
+    }
+    return 0;
+}
+
